219255a10f57d9d1d9d567bfe2a8f703d18ddafa
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 10:54:57 -0500

    place values challenge

---
+++ place_values.nim
@@ -0,0 +1,14 @@
+import algorithm
+import strutils
+import math
+
+stdout.write("> ")
+let inp: string = readLine(stdin)
+var places: seq[float]
+
+for i in countdown(len(inp)-1, 0):
+  places.add(float64(strutils.parseInt($inp[i])) * math.pow(10, float64(len(inp)-i-1)))
+
+algorithm.reverse(places)
+
+echo repr(places)
