5ef64abcd8675914363a71b6630298ae88defa42
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 09:22:30 -0500

    atbash in nimmmmm

---
+++ atbash.nim
@@ -0,0 +1,14 @@
+import strutils
+
+let a: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var s: string = readLine(stdin)
+var result: string
+
+for item in s:
+  if strutils.isLowerAscii(item):
+    result = result & a[len(a) - strutils.find(a, strutils.toUpperAscii(item)) - 1]
+  else:
+    result = result & a[len(a) - strutils.find(a, strutils.toLowerAscii(item)) - 1]
+
+echo result
