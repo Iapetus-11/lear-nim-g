d70f07567a4c9d196fe7b04d4bd6e9f288af09b1
Milo Weinberg <iapetus011@gmail.com>
2021-08-12 21:46:00 -0400

    create fibonacciHashmapBig.nim which uses bigints package

---
+++ fibonacciHashmapBig.nim
@@ -0,0 +1,28 @@
+import strutils
+import bigints
+import tables
+import hashes
+import math
+
+stdout.write("How many numbers of the Fibonacci sequence to calculate? ")
+let n: int = strutils.parseInt(readLine(stdin))
+
+var fibonacciCache: Table[BigInt, BigInt] = initTable[BigInt, BigInt]()
+
+proc hash(n: BigInt): int = hash($n)
+
+proc calculateFibonacci(x: BigInt): BigInt =
+    if fibonacciCache.hasKey(x):
+        return fibonacciCache[x]
+
+    if x == 0:
+        return initBigInt(0)
+
+    if x == 1 or x == 2:
+        return initBigInt(1)
+
+    result = calculateFibonacci(x-1) + calculateFibonacci(x-2)
+    fibonacciCache[x] = result
+
+for i in 0..n:
+    echo "[", i, "]: ", calculateFibonacci(initBigInt(i))
