0e19eeefbdda176edb30fb9d0d022f701ab4a38f
Milo Weinberg <iapetus011@gmail.com>
2021-08-12 20:46:19 -0400

    add maxLookAndSay

---
+++ maxLookAndSay.nim
@@ -0,0 +1,28 @@
+import strutils
+import sequtils
+
+proc countCurrent(current: char, n: string): int =
+    for c in n:
+        if c == current:
+            result += 1
+        else:
+            break
+
+proc lookAndSay(n: string): string =
+    var i = 0
+
+    while i < n.len:
+        let count = countCurrent(n[i], n[i..n.high])
+        result &= $count & n[i]
+        i += count
+
+
+when isMainModule:
+    stdout.write("How many iterations? ")
+    let iterations = parseInt(readLine(stdin))
+
+    var current = "1"
+    
+    for i in 0..iterations:
+        echo current.toSeq.max
+        current = lookAndSay(current)
