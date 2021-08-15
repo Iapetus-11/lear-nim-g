e8e525476473f0ea080ab8284c83e2f4dcea5cde
Milo Weinberg <iapetus011@gmail.com>
2021-08-15 15:31:19 -0400

    add recaman and recamanTable.nim

---
+++ recaman.nim
@@ -0,0 +1,37 @@
+import strformat
+import strutils
+import times
+import sets
+import os
+
+# Uses a seq (aka mutable list) and a HashSet, It's slightly
+# faster than recamanTable.nim but uses more memory I think
+proc recaman(n: int): seq[int] =
+    var already = [0].toHashset
+
+    for i in 0..n-1:
+        result.add(0)
+
+    for i in 1..n-1:
+        var c = result[i - 1] - i
+
+        if c < 0 or (c in already):
+            c = result[i - 1] + i
+        
+        result[i] = c
+        already.incl(c)
+
+when isMainModule:
+    var n: int
+
+    try:
+        n = parseInt(paramStr(1))
+    except IndexDefect:
+        stdout.write("How many iterations? ")
+        n = parseInt(readLine(stdin))
+        
+    let startTime = getTime()
+    let result = recaman(n)
+    let duration = inMilliSeconds(getTime() - startTime).toBiggestFloat / 1000
+
+    echo result[result.high], &" ({duration:.2f} seconds)"
---
+++ recamanTable.nim
@@ -0,0 +1,33 @@
+import strformat
+import strutils
+import tables
+import times
+import os
+
+# Slightly slower than recaman.nim (around 1.15x I think), but uses much less memory
+proc recamanTable(n: int): Table[int, int] =
+    for i in 0..n-1:
+        result[i] = 0
+
+    for i in 1..n-1:
+        var c = result[i - 1] - i
+
+        if c < 0 or (n in result):
+            c = result[i - 1] + i
+
+        result[i] = c
+
+when isMainModule:
+    var n: int
+
+    try:
+        n = parseInt(paramStr(1))
+    except IndexDefect:
+        stdout.write("How many iterations? ")
+        n = parseInt(readLine(stdin))
+        
+    let startTime = getTime()
+    let result = recamanTable(n)
+    let duration = inMilliSeconds(getTime() - startTime).toBiggestFloat / 1000
+
+    echo result[result.len-1], &" ({duration:.2f} seconds)"
