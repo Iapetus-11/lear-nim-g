c98262d4a7971f3075c44a70b2f4c9d071345d51
Milo Weinberg <iapetus011@gmail.com>
2021-08-14 23:19:13 -0400

    sus

---
+++ whenTheGuiIsSus.nim
@@ -0,0 +1,44 @@
+import random; randomize()
+import nigui; app.init()
+
+var
+    window = newWindow()
+    control = newControl()
+
+window.width = 500
+window.height = 500
+
+window.add(control)
+
+control.widthMode = WidthMode_Fill
+control.heightMode = HeightMode_Fill
+
+control.onDraw = proc (event: DrawEvent) =
+    var c = "r"
+
+    while true:
+        let canvas = control.canvas
+
+        canvas.areaColor = rgb(0, 0, 0)
+        canvas.fill()
+        
+        case c:
+        of "r":
+            canvas.textColor = rgb(255, 0, 0)
+            c = "g"
+        of "g":
+            canvas.textColor = rgb(0, 255, 0)
+            c = "b"
+        else:
+            canvas.textColor = rgb(0, 0, 255)
+            c = "r"
+
+        canvas.fontSize = 96
+        canvas.fontFamily = "Impact"
+        canvas.drawText("SUS", 150, 150)
+
+        app.sleep(1)
+
+
+window.show()
+app.run()
