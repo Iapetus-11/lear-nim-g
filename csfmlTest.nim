a62f943f2a98131311c80c97ecf2063d3477eb57
Milo Weinberg <iapetus011@gmail.com>
2021-08-17 12:40:00 -0400

    threads test in csfmlTest.nim

--- csfmlTest.nim
+++ csfmlTest.nim
@@ -1,9 +1,53 @@
 import csfml
 
-var
-    window = newRenderWindow(videoMode(800, 600), "My Window")
+const BACKGROUND_COLOR = color(30, 30, 40)
+const WINDOW_X = 800
+const WINDOW_Y = 600
 
+var ctxSettings = ContextSettings()
+ctxSettings.antialiasingLevel = 4
+
+var window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "My Window", settings=ctxSettings)
 window.verticalSyncEnabled = true
+
+proc setActiveUnsafe(w: RenderWindow, active: bool): bool =
+    result = block:
+        window.active = active
+
+proc setActive(w: RenderWindow, active: bool) {.discardable.} =
+    let success = setActiveUnsafe(w, active)
+
+    if not success:
+        raise newException(CatchableError, "Unable to set active window state.")
+
+var drawThread: system.Thread[void]
+
+proc drawInThread() {.thread, gcsafe.} =
+    window.setActive(true)
+
+    window.clear(BACKGROUND_COLOR)
+
+    let
+        color = color(232, 0, 255)
+        origin = vec2(600, 200)
+        points = [
+            origin,
+            origin + vec2(10, 0),
+            origin + vec2(10, 10),
+            origin + vec2(20, 10)
+        ]
+
+    var vertices = newVertexArray(LineStrip, 10)
+    for i in 0..points.high:
+        vertices[i] = vertex(points[i], color)
+
+    window.draw(vertices)
+    vertices.destroy()
+
+    window.display()
+
+window.setActive(false)
+createThread(drawThread, drawInThread)
 
 while window.open:
     var event: Event
