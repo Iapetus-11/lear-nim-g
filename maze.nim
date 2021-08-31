a8f65d362e8f7471f0ec6b8952f1ec350e4829ba
Milo Weinberg <iapetus011@gmail.com>
2021-08-31 12:35:15 -0400

    maze progress

--- maze.nim
+++ maze.nim
@@ -19,6 +19,11 @@ proc doMaze(sizeX: int, sizeY: int) =
         for y in 0..sizeY-1:
             maze[x].add(true)
 
+    maze[1][1] = false
+    maze[maze.high-1][maze[0].high-1] = false
+    maze[(maze.high/2).toInt][(maze[0].high/2).toInt] = false
+    maze[(maze.high/3).toInt][(maze[0].high/3).toInt] = false
+
     proc fill(x: int, y: int) =
         let leftWall = maze[x-1][y-1] and maze[x-1][y] and maze[x-1][y+1]
         let rightWall = maze[x+1][y-1] and maze[x+1][y] and maze[x+1][y+1]
@@ -27,16 +32,20 @@ proc doMaze(sizeX: int, sizeY: int) =
         let bottomWall = maze[x-1][y-1] and maze[x][y-1] and maze[x+1][y-1]
 
         if (leftWall and rightWall) or (topWall and bottomWall) or (leftWall and topWall) or (rightWall and topWall) or (leftWall and bottomWall) or (rightWall and bottomWall):
-            if true or (maze[x-1][y] and maze[x+1][y]) or (maze[x][y-1] and maze[x][y+1]):
+            if not (maze[x-1][y] and maze[x][y+1] and maze[x+1][y] and maze[x][y-1]):
                 maze[x][y] = false
 
     for x in 1..sizeX-2:
         for y in 1..sizeY-2:
-            if rand(100) > 10:
+            if rand(100) > 1:
                 # if (maze[x-1][y] and maze[x+1][y]) or (maze[x][y-1] and maze[x][y+1]):
                     # maze[x][y] = false
 
                 fill(x, y)
+                fill(sizeX-x-1, sizeY-y-1)
+    
+    for i in 0..(sizeX*sizeY*2):
+        fill(rand(sizeX-3)+1, rand(sizeY-3)+1)
     
     for x in 0..sizeX-1:
         for y in 0..sizeY-1:
