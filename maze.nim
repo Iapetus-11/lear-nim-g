8d67a368dbb1bb3b81b376ce7e91884ec65f93a8
Milo Weinberg <iapetus011@gmail.com>
2021-08-31 16:25:31 -0400

    start from scratch again

--- maze.nim
+++ maze.nim
@@ -1,75 +1,108 @@
 import random; randomize()
-# import csfml
 
-# const BACKGROUND_COLOR = color(30, 30, 40)
-# const WINDOW_X = 800
-# const WINDOW_Y = 600
+type Maze = seq[seq[char]]
 
-# var ctxSettings = ContextSettings()
-# ctxSettings.antialiasingLevel = 16
+proc drawMaze(maze: Maze) =
+    for y in 0..maze.high:
+        for x in 0..maze[y].high:
+            stdout.write(maze[y][x])
 
-# var window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "My Window", settings=ctxSettings)
-# window.verticalSyncEnabled = true
-
-proc doMaze(sizeX: int, sizeY: int) =
-    var maze = newSeq[seq[bool]]()
-
-    for x in 0..sizeX-1:
-        maze.add(newSeq[bool]())
-        for y in 0..sizeY-1:
-            maze[x].add(true)
-
-    maze[1][1] = false
-    maze[maze.high-1][maze[0].high-1] = false
-    maze[(maze.high/2).toInt][(maze[0].high/2).toInt] = false
-    maze[(maze.high/3).toInt][(maze[0].high/3).toInt] = false
-
-    proc fill(x: int, y: int) =
-        let leftWall = maze[x-1][y-1] and maze[x-1][y] and maze[x-1][y+1]
-        let rightWall = maze[x+1][y-1] and maze[x+1][y] and maze[x+1][y+1]
-
-        let topWall = maze[x-1][y+1] and maze[x][y+1] and maze[x+1][y+1]
-        let bottomWall = maze[x-1][y-1] and maze[x][y-1] and maze[x+1][y-1]
-
-        if (leftWall and rightWall) or (topWall and bottomWall) or (leftWall and topWall) or (rightWall and topWall) or (leftWall and bottomWall) or (rightWall and bottomWall):
-            if not (maze[x-1][y] and maze[x][y+1] and maze[x+1][y] and maze[x][y-1]):
-                maze[x][y] = false
-
-    for x in 1..sizeX-2:
-        for y in 1..sizeY-2:
-            if rand(100) > 1:
-                # if (maze[x-1][y] and maze[x+1][y]) or (maze[x][y-1] and maze[x][y+1]):
-                    # maze[x][y] = false
-
-                fill(x, y)
-                fill(sizeX-x-1, sizeY-y-1)
-    
-    for i in 0..(sizeX*sizeY*2):
-        fill(rand(sizeX-3)+1, rand(sizeY-3)+1)
-    
-    for x in 0..sizeX-1:
-        for y in 0..sizeY-1:
-            var o = " "
-            
-            if maze[x][y]:
-                o = "█"
-
-            stdout.write(o)
         stdout.writeLine("")
 
-doMaze(20, 60)
+proc newMaze(sizeX: int, sizeY: int): Maze =  # blank spaces
+    for x in 0..sizeX-1:
+        result.add(newSeq[char]())
+        for y in 0..sizeY-1:
+            result[x].add('u')
 
-# while window.open:
-#     var event: Event
+proc countSurroundingCells(maze: Maze, wall: tuple[x: int, y: int]): int =
+    result = 0
 
-#     while window.pollEvent(event):
-#         case event.kind:
-#         of EventType.Closed: window.close()
-#         of EventType.KeyPressed:
-#             if event.key.code == KeyCode.Escape:
-#                 window.close()
-#             else:
-#                 echo event.key.code
-#         else: discard
+    if maze[wall.y-1][wall.x] == 'c':
+        result += 1
+    
+    if maze[wall.y+1][wall.x] == 'c':
+        result += 1
 
-# window.destroy()
+    if maze[wall.y][wall.x-1] == 'c':
+        result += 1
+
+    if maze[wall.y][wall.x+1] == 'c':
+        result += 1
+
+proc doMaze(sizeX: int, sizeY: int) =
+    var maze = newMaze(sizeX, sizeY)
+    var walls: seq[tuple[x: int, y: int]]
+
+    let start = (x: 2, y: maze.high-1)
+
+    walls.add((start.y-1, start.x))
+    walls.add((start.y, start.x-1))
+    walls.add((start.y, start.x+1))
+    walls.add((start.y+1, start.x))
+
+    maze[start.y-1][start.x] = 'w'
+    maze[start.y][start.x-1] = 'w'
+    maze[start.y][start.x+1] = 'w'
+    maze[start.y+1][start.x] = 'w'
+
+    proc addWall(wall: tuple[x: int, y: int]) =
+        if countSurroundingCells(maze, wall) < 2:
+            maze[wall.y][wall.x] = 'c'
+
+            if wall.y != 0:
+                if maze[wall.y-1][wall.x] != 'c':
+                    maze[wall.y-1][wall.x] = 'w'
+
+                if not walls.contains((x: wall.x, y: wall.y-1)):
+                    walls.add((x: wall.x, y: wall.y-1))
+
+    while walls.len > 0:
+        let randomWall = walls[(rand(100).toFloat/100.0 * walls.high.toFloat).toInt]
+        echo randomWall
+
+        if randomWall.x != 0:
+            if maze[randomWall.y][randomWall.x-1] == 'u' and maze[randomWall.y-1][randomWall.x] == 'c':
+                addWall(randomWall)
+                walls.delete(walls.find(randomWall))
+            
+            continue
+
+        if randomWall.y != 0:
+            if maze[randomWall.y-1][randomWall.x] == 'u' and maze[randomWall.y+1][randomWall.x+1] == 'c':
+                addWall(randomWall)
+                walls.delete(walls.find(randomWall))
+            
+            continue
+
+        if randomWall.y != maze.high:
+            if maze[randomWall.y+1][randomWall.x] == 'u' and maze[randomWall.y-1][randomWall.x] == 'c':
+                addWall(randomWall)
+                walls.delete(walls.find(randomWall))
+            
+            continue
+
+        if randomWall.x != maze[0].high:
+            if maze[randomWall.y][randomWall.x+1] == 'u' and maze[randomWall.y][randomWall.x-1] == 'c':
+                addWall(randomWall)
+                walls.delete(walls.find(randomWall))
+            
+            continue
+
+    for y in 0..sizeY-1:
+        for x in 0..sizeX-1:
+            if maze[y][x] == 'u':
+                maze[y][x] = 'w'
+
+    drawMaze(maze)
+
+
+
+
+
+
+
+    drawMaze(maze)
+    
+
+doMaze(10, 30)
