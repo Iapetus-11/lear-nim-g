c414b110b8ba01e1895475eac7558d1e2627b484
Milo Weinberg <iapetus011@gmail.com>
2021-09-04 13:19:38 -0400

    add implementation of a circular sequence

---
+++ circularSeq.nim
@@ -0,0 +1,37 @@
+type CircleSeq[T] = ref object of RootObj
+    s: seq[T]
+
+
+proc `[]`[T](s: CircleSeq[T], i: int): T =
+    return s.s[i mod s.s.len]
+
+proc `[]=`[T](s: CircleSeq[T], i: int, value: T) =
+    s.s[i mod s.s.len] = value
+
+proc add[T](s: CircleSeq[T], value: T) =
+    s.s.add(value)
+
+proc len[T](s: CircleSeq[T]): int =
+    return s.s.len
+
+proc `$`[T](s: CircleSeq[T]): string =
+    return "CircleSeq(" & $s.s & ")"
+
+proc initCircleSeq[T](): CircleSeq[T] =
+    return CircleSeq[T](s: newSeq[T]())
+
+when isMainModule:
+    var s = initCircleSeq[char]()
+
+    echo s
+
+    s.add('a')
+    s.add('b')
+    s.add('c')
+    s.add('D')
+
+    echo s
+
+    for i in 0..s.len*2:
+        echo i, ": ", s[i]
+
