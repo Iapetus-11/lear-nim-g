995cc519fb63a8eb22a49ffa133974d5253b6217
Milo Weinberg <iapetus011@gmail.com>
2021-09-07 14:01:40 -0400

    progress

--- stockPriceVisualizer.nim
+++ stockPriceVisualizer.nim
@@ -1,3 +1,4 @@
+import std/[strformat, os, json, strutils, tables]
 import csfml
 
 const
@@ -9,6 +10,26 @@ proc isNan(n: float): bool =
     return not (n > 0 or n < 0 or n == 0)
 
 let
+    stock = paramStr(1).toUpperAscii()
+    stockPrices = block:
+        let
+            data = parseJson(readFile(&"dump/{stock.toUpperAscii()}.json"))
+            fields = data.getFields()
+
+        var
+            result = newSeq[float]()
+            started = false
+
+        for p in fields.values:
+            let pf = p.getFloat()
+
+            if (isNan(pf) or pf == 0.0) and started:
+                result.add(0)
+            else:
+                started = true
+                result.add(pf)
+
+        result
     ctxSettings = ContextSettings(antialiasingLevel: 16)
     window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "My Window", settings = ctxSettings)
 
