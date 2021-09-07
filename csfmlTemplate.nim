5ee7d3c14d5308073fbda5f0c5ba293059583bce
Milo Weinberg <iapetus011@gmail.com>
2021-09-07 13:52:27 -0400

    update csfmlTemplate.nim

--- csfmlTemplate.nim
+++ csfmlTemplate.nim
@@ -4,21 +4,25 @@ const BACKGROUND_COLOR = color(30, 30, 40)
 const WINDOW_X = 800
 const WINDOW_Y = 600
 
-var ctxSettings = ContextSettings()
-ctxSettings.antialiasingLevel = 16
+let
+    ctxSettings = ContextSettings(antialiasingLevel: 16)
+    window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "My Window", settings = ctxSettings)
 
-var window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "My Window", settings = ctxSettings)
 window.verticalSyncEnabled = true
 
-while window.open:
-    var event: Event
+var
+    event: Event
 
-    while window.pollEvent(event):
+while window.open:
+    if window.pollEvent(event):
         case event.kind:
-        of EventType.Closed: window.close()
+        of EventType.Closed:
+            window.close()
+            break
         of EventType.KeyPressed:
             if event.key.code == KeyCode.Escape:
                 window.close()
+                break
             else:
                 echo event.key.code
         else: discard
