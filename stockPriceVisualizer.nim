3df0a6025e8ef0b8d80ec398704eca34c7cc3b1a
Milo Weinberg <iapetus011@gmail.com>
2021-09-07 17:02:10 -0400

    improvements! Smoothing, zoom, color!

--- stockPriceVisualizer.nim
+++ stockPriceVisualizer.nim
@@ -6,23 +6,40 @@ const
     WINDOW_X: cint = 800
     WINDOW_Y: cint = 600
 
+iterator enumerate[T](s: seq[T]): tuple[i: int, v: T] =
+    var i = 0
+
+    for e in s:
+        yield (i, e)
+        i += 1
+
 proc isNan(n: float): bool =
     return not (n > 0 or n < 0 or n == 0)
 
-proc drawStonk(w: RenderWindow, d: seq[float], m: float, y1: int, y2: int) =
+proc smoothedMax(s: seq[float]): float =
+    let mid = s.high / 2
+
+    for i, e in enumerate(s):
+        var adj = (abs(mid - float(i)) + 1.0) / float(s.len) * 5.0
+        result += e / adj
+
+    result /= float(s.len)
+
+proc drawStonk(w: RenderWindow, d: seq[float], m: float, c: tuple[x1: int, x2: int, y1: int, y2: int]) =
     let
         l = d.len
-        m = d.max
-        widthAdjust = float(WINDOW_X) / float(l)
-        heightAdjust = float(y2) / m
+        widthAdjust = float(c.x2 - c.x1) / float(l)
+        heightAdjust = float(c.y2 - c.y1) / m
 
-    var
-        vertices = newVertexArray(PrimitiveType.LineStrip, l)
+    var vertices = newVertexArray(PrimitiveType.LineStrip, l)
 
     for i in 0..l-1:
         vertices[i] = vertex(
-            vec2(float(i) * widthAdjust, float(WINDOW_Y) - (float(y1) + d[i] * heightAdjust)),
-            color(10, 115, 255)
+            vec2(
+                float(i) * widthAdjust + float(c.x1),
+                float(c.y2 - c.y1) - d[i] * heightAdjust + float(c.y1)
+            ),
+            color(20, 255, 255)
         )
 
     w.draw(vertices)
@@ -37,25 +54,30 @@ let
 
         var
             result = newSeq[float]()
-            started = false
+            started = false  # should be false in prod
 
         for p in fields.values:
             let pf = p.getFloat()
 
-            if (isNan(pf) or pf == 0.0) and started:
-                result.add(0)
+            if isNan(pf) or pf == 0.0:
+                if started:
+                    result.add(0)
             else:
                 started = true
                 result.add(pf)
 
         result
     stockPricesMax = stockPrices.max
+    stockPricesLen = stockPrices.len
 
 var
     ctxSettings = ContextSettings(antialiasingLevel: 16)
     window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), &"Stock Price Visualization [{stock}]",
             WindowStyle.Default, ctxSettings)
     event: Event
+    rDefault = 0..stockPricesLen-1
+    r = rDefault
+    rOuter = rDefault
 
 window.verticalSyncEnabled = true
 
@@ -72,7 +94,17 @@ while window.open:
         else: discard
 
     window.clear(BACKGROUND_COLOR)
-    window.drawStonk(stockPrices, stockPricesMax, 10, 200)
+    window.drawStonk(stockPrices, stockPricesMax, (20, int(WINDOW_X) - 20, 20, int(WINDOW_Y / 2) - 20))
+
+    let mPos = window.mouse_getPosition
+
+    if mPos.x <= 10 or mPos.x >= WINDOW_X - 10:
+        window.drawStonk(stockPrices, stockPricesMax, (20, int(WINDOW_X) - 20, int(WINDOW_Y / 2) + 20, int(WINDOW_Y) - 20))
+    else:
+        r = max(int(mPos.x-15), 0)..min(int(mPos.x)+14, WINDOW_X)
+        rOuter = max(int(mPos.x)-119, 0)..min(int(mPos.x)+120, WINDOW_X)
+        window.drawStonk(stockPrices[r], stockPrices[rOuter].smoothedMax, (20, int(WINDOW_X) - 20, int(WINDOW_Y / 2) + 20, int(WINDOW_Y) - 20))
+    
     window.display()
 
 window.destroy()
