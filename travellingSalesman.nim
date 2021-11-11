96b8c984d8eaccaa5308dac738676de3077e7564
Milo Weinberg <iapetus011@gmail.com>
2021-11-11 09:31:48 -0500

    progress

--- travellingSalesman.nim
+++ travellingSalesman.nim
@@ -1,7 +1,7 @@
 # Travelling Salesman Problem with ant colony optimization
 # inspired by Sebastian Lague - https://www.youtube.com/watch?v=X-iSQQgOd1A
 
-import std/random
+import std/[random, math, algorithm]
 import csfml
 
 randomize()
@@ -15,6 +15,12 @@ const
     BACKGROUND_COLOR = color(30, 30, 40)
     WINDOW_X: cint = 800
     WINDOW_Y: cint = 600
+
+proc distance(a: Point, b: Point): float =
+    return sqrt(pow(float(b.x - a.x), 2.0) + pow(float(b.y - a.y), 2.0))
+
+proc `$`(point: Point): string =
+    return '(' & $point.x & ',' & $point.y & ')'
 
 proc vec2(point: Point): Vector2i =
     return vec2(point.x, point.y)
@@ -32,25 +38,69 @@ proc drawPoints(window: RenderWindow, points: seq[Point], color: Color, radius: 
         let c = newCircleShape(radius, pointsPerCircle)
 
         c.fillColor = color
-        c.position = vec2(p)
+        c.position = vec2(p.x.float - radius, p.y.float - radius)
 
         window.draw(c)
-
         c.destroy()
 
-proc solveBruteForceBlocking(window: RenderWindow, points: seq[Point]) =
+proc drawLine(window: RenderWindow, a: Point, b: Point, color: Color) =
+    let vertices = newVertexArray(Lines, 2)
+    
+    vertices.append(vertex(vec2(a), color))
+    vertices.append(vertex(vec2(b), color))
+
+    window.draw(vertices)
+    vertices.destroy()
+
+proc drawLinesIncr(window: RenderWindow, lines: seq[array[2, Point]], color: Color, delayMs: int) =
+    for i in 0..lines.high:
+        if i != 0:
+            sleep(milliseconds(delayMs.int32))
+
+        window.drawLine(lines[i][0], lines[i][1], color)
+        window.display()
+
+proc getOptimalPath(window: RenderWindow, points: seq[Point]): seq[Point] =
+    if points.len == 0:
+        return
+
     let start = points[0]
+    var
+        lowestDistance = distance(start, points[1])
+        nearestPoint = points[1]
+        nearestPointIdx = 1
+
+    for i in 2..points.high:
+        let d = distance(start, points[i])
+
+        if lowestDistance > d:
+            lowestDistance = d
+            nearestPoint = points[i]
+            nearestPointIdx = i
+
+    result.add(start)
+    result.add(nearestPoint)
+
+    var newPoints = points
+    newPoints.del(nearestPointIdx)
+    newPoints.insert(nearestPoint, 0)
+
+    for p in getOptimalPath(window, newPoints):
+        result.add(p)
+    
+
+
 
 let
     ctxSettings = ContextSettings(antialiasingLevel: 16)
-    window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "Travelling Salesman (Ants)",
+    window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "Travelling Salesman",
             settings = ctxSettings)
 
 window.verticalSyncEnabled = true
 
 var
     event: Event
-    points = genPoints(20, 50)
+    points = genPoints(6, 50)
 
 while window.open:
     if window.pollEvent(event):
@@ -64,12 +114,13 @@ while window.open:
                 window.close()
                 break
             of KeyCode.R:
-                points = genPoints(10, 50)
+                points = genPoints(6, 50)
             else: discard
         else: discard
 
     window.clear(BACKGROUND_COLOR)
     window.drawPoints(points, color(255, 30, 50), 4.0, 10)
+    window.getPaths(points)
 
     window.display()
 
