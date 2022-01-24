537bcd3276403fdb9e984a26bf20dcb71acf3f21
Milo Weinberg <iapetus011@gmail.com>
2022-01-24 11:25:06 -0500

    start on simulation of rope + fabrics

---
+++ simtest.nim
@@ -0,0 +1,77 @@
+import nico
+
+import simtestvec
+
+type
+    Point = ref object
+        pos, prevPos: PVec2
+        locked: bool
+
+    Stick = ref object
+        a, b: Point
+        len: Pfloat
+
+const
+    WINDOW_X = 512
+    WINDOW_Y = 512
+    SCALE = 1
+
+    FPS = 60
+
+    GRAVITY = 0.0098
+    STICK_SIM_ITERS = 2
+
+var
+    points: seq[Point]
+    sticks: seq[Stick]
+
+proc gameInit() =
+    setPalette(loadPalettePico8())
+
+    points.setLen(0)
+    sticks.setLen(0)
+
+proc simulate(dt: float32) =
+    for p in points:
+        if not p.locked:
+            let prevPos = p.pos
+
+            p.pos += p.pos - p.prevPos
+            p.pos += vec2(0, 1) * (GRAVITY * dt * dt)
+            p.prevPos = prevPos
+
+    for i in 0 .. STICK_SIM_ITERS - 1:
+        for s in sticks:
+            let
+                sCenter = (s.a.pos + s.b.pos) / 2
+                sDir = (s.a.pos - s.b.pos).norm
+
+            if not s.a.locked:
+                s.a.pos = sCenter + sDir * s.len / 2
+
+            if not s.b.locked:
+                s.b.pos = sCenter - sDir * s.len / 2
+
+proc controls() =
+    discard
+
+proc gameUpdate(dt: float32) =
+    simulate(dt)
+    controls()
+
+proc gameDraw() =
+    cls()
+
+    setColor(1)
+    for s in sticks:
+        line(s.a.pos.x, s.a.pos.y, s.b.pos.x, s.b.pos.y)
+
+    setColor(12)
+    for p in points:
+        circfill(p.pos.x, p.pos.y, 10)
+
+nico.timeStep = 1 / FPS  # set fps
+
+nico.init("me.iapetus11", "simtest")
+nico.createWindow("Simulation Test", WINDOW_X, WINDOW_Y, SCALE, false)
+nico.run(gameInit, gameUpdate, gameDraw)
---
+++ simtestvec.nim
@@ -0,0 +1,61 @@
+import std/[math]
+import nico
+
+type
+  PVec2* = ref object
+    x*: PFloat
+    y*: PFloat
+
+proc vec2*(x: Pfloat, y: PFloat): PVec2 =
+  return PVec2(x: x, y: y)
+
+proc vec2*(p: tuple[x: int, y: int]): PVec2 =
+  return PVec2(x: p.x.float, y: p.y.float)
+
+proc `$`*(p: PVec2): string =
+  return "(" & $p.x & "," & $p.y & ")"
+
+proc `+`*(a: PVec2, b: PVec2): PVec2 =
+  return vec2(a.x + b.x, a.y + b.y)
+
+proc `+=`*(a: PVec2, b: PVec2) =
+  a.x += b.x
+  a.y += b.y
+
+proc `-`*(a: PVec2, b: PVec2): PVec2 =
+  return vec2(a.x - b.x, a.y - b.y)
+
+proc `-=`*(a: PVec2, b: PVec2) =
+  a.x -= b.x
+  a.y -= b.y
+
+proc `*`*(a: PVec2, b: Pfloat): Pvec2 =
+    result = PVec2()
+    result.x = a.x * b
+    result.y = a.y * b
+
+proc `*=`*(a: PVec2, b: Pfloat) =
+    a.x = a.x * b
+    a.y = a.y * b
+
+proc `/`*(a: PVec2, b: Pfloat): PVec2 =
+  result = PVec2()
+  result.x = a.x / b
+  result.y = a.y / b
+
+proc `/=`*(a: PVec2, b: Pfloat) =
+  a.x = a.x / b
+  a.y = a.y / b
+
+proc rot*(p: PVec2, deg: Pfloat): PVec2 =
+  let rad = degToRad(deg)
+
+  return vec2(p.x * cos(rad) - p.y * sin(rad), p.x * sin(rad) + p.y * cos(rad))
+
+proc mag*(v: PVec2): Pfloat =
+    return sqrt(v.x * v.x + v.y * v.y)
+
+proc norm*(v: PVec2): PVec2 =
+    let m = v.mag
+    
+    return vec2(v.x / m, v.y / m)
